31e06dbdd3b26f067a866351befa0e027ae65cac
Aditya-Chowdhary <adityachowdhary04@gmail.com>
2024-01-22 15:32:32 +0530

    feat: graceful shutdown of server

--- fanfiction-backend/cmd/api/healthcheck.go
+++ fanfiction-backend/cmd/api/healthcheck.go
@@ -7,7 +7,7 @@ import (
 // healthcheckHandler is a basic request used to check the status of the server
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
 	data := envelope{
-		"status":      "available",
+		"status": "available",
 		"system_info": map[string]string{
 			"version":     version,
 			"environment": app.config.env,
