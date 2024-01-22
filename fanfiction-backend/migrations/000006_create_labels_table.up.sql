bb54106c3f6a855aa2e552aae31e4ab464fa0834
Aditya-Chowdhary <adityachowdhary04@gmail.com>
2024-01-22 18:00:03 +0530

    fix: Reorder migration files

--- fanfiction-backend/migrations/000006_create_labels_table.up.sql
+++ fanfiction-backend/migrations/000006_create_labels_table.up.sql
@@ -5,8 +5,10 @@ CREATE TABLE IF NOT EXISTS labels
 (
     id bigserial,
     created_at timestamp with time zone NOT NULL DEFAULT NOW(),
+    story_id bigint NOT NULL,
     name citext NOT NULL,
     version integer NOT NULL DEFAULT 1,
     PRIMARY KEY (id),
+    FOREIGN KEY (story_id) REFERENCES stories (id) MATCH SIMPLE ON DELETE CASCADE,
     UNIQUE (name)
 );
