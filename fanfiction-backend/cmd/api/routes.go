ea61ecb3376c3c0b1e4c4514a2fc7cfa52f4839c
Aditya-Chowdhary <adityachowdhary04@gmail.com>
2024-01-22 17:25:09 +0530

    feat: forgot password

--- fanfiction-backend/cmd/api/routes.go
+++ fanfiction-backend/cmd/api/routes.go
@@ -54,7 +54,9 @@ func (app *application) routes() http.Handler {
 	
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
 	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
+	router.HandlerFunc(http.MethodPut, "/v1/users/password", app.updateUserPasswordHandler)
 
 	router.HandlerFunc(http.MethodPost, "/v1/tokens/authentication", app.createAuthTokenHandler)
+	router.HandlerFunc(http.MethodPost, "/v1/tokens/password-reset", app.createPasswordResetTokenHandler)
 	return app.recoverPanic(app.authenticate(router))
 }
--- fanfiction-backend/cmd/api/tokensHandlers.go
+++ fanfiction-backend/cmd/api/tokensHandlers.go
@@ -11,7 +11,7 @@ import (
 
 func (app *application) createAuthTokenHandler(w http.ResponseWriter, r *http.Request) {
 	var input struct {
-		Email string `json:"email"`
+		Email    string `json:"email"`
 		Password string `json:"password"`
 	}
 
@@ -34,8 +34,8 @@ func (app *application) createAuthTokenHandler(w http.ResponseWriter, r *http.Re
 	if err != nil {
 		switch {
 		case errors.Is(err, data.ErrRecordNotFound):
-			app.invalidCredentialsResponse(w,r)
-		default: 
+			app.invalidCredentialsResponse(w, r)
+		default:
 			app.serverErrorResponse(w, r, err)
 		}
 		return
@@ -52,13 +52,74 @@ func (app *application) createAuthTokenHandler(w http.ResponseWriter, r *http.Re
 		return
 	}
 
-	token, err := app.models.Tokens.New(user.ID, 24 * time.Hour, data.ScopeAuthentication)
+	token, err := app.models.Tokens.New(user.ID, 24*time.Hour, data.ScopeAuthentication)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	err = app.writeJSON(w, http.StatusCreated, envelope{"authentication_token": token}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+}
+
+func (app *application) createPasswordResetTokenHandler(w http.ResponseWriter, r *http.Request) {
+	var input struct {
+		Email string `json:"email"`
+	}
+
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	v := validator.New()
+	if data.ValidateEmail(v, input.Email); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	user, err := app.models.Users.GetUserByEmail(input.Email)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			v.AddError("email", "no matching email address found")
+			app.failedValidationResponse(w, r, v.Errors)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	if !user.Activated {
+		v.AddError("email", "user account must be activated")
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	token, err := app.models.Tokens.New(user.ID, 45 * time.Minute, data.ScopePasswordReset)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 		return
 	}
 	
-	err = app.writeJSON(w, http.StatusCreated, envelope{"authentication_token": token}, nil)
+	app.background(func() {
+		data := map[string]interface{}{
+			"passwordResetToken": token.Plaintext,
+		}
+
+		err = app.mailer.Send(user.Email, "token_password_reset.tmpl", data)
+		if err != nil {
+			app.logger.PrintError(err, nil)
+		}
+	})
+
+	env := envelope{"message": "an email will be sent to you containing password reset instructions"}
+
+	err = app.writeJSON(w, http.StatusCreated, env, nil)
 	if err != nil {
 		app.serverErrorResponse(w, r, err)
 		return
