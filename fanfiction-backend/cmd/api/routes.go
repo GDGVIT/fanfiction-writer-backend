ae11150cb2670ad91762a6bae2abed2d8b1c1fc0
Aditya-Chowdhary <adityachowdhary04@gmail.com>
2024-02-22 02:02:58 +0530

    fix: temp fix

--- fanfiction-backend/cmd/api/routes.go
+++ fanfiction-backend/cmd/api/routes.go
@@ -40,7 +40,9 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodPost, "/v1/events", app.requireActivatedUser(app.createEventHandler))
 	router.HandlerFunc(http.MethodGet, "/v1/events/:id", app.requireActivatedUser(app.getEventHandler))
 	router.HandlerFunc(http.MethodGet, "/v1/events", app.requireActivatedUser(app.listEventHandler))
-	router.HandlerFunc(http.MethodGet, "/v1/eventsstory", app.requireActivatedUser(app.listStoryEventHandler))
+	// router.HandlerFunc(http.MethodGet, "/v1/eventsstory", app.requireActivatedUser(app.listStoryEventHandler))
+	// ! Change this back
+	router.HandlerFunc(http.MethodGet, "/v1/eventsstory", app.listStoryEventHandler) 
 	router.HandlerFunc(http.MethodDelete, "/v1/events/:id", app.requireActivatedUser(app.deleteEventHandler))
 	router.HandlerFunc(http.MethodPatch, "/v1/events/:id", app.requireActivatedUser(app.updateEventHandler))
 
